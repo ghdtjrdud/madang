cab74560aa1dbd5f80276ae6d95bb7a4a2c2a922
ghdtjrdud <ghdtjrdud38@gmail.com>
2024-09-24 10:31:13 +0900

    [Add]수정

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.entity.Book;
 import com.example.demo.service.BookService;
@@ -28,6 +29,12 @@ public class BookController {
 	
 	public void list(Model model) {
 		model.addAttribute("list",bs.findAll());
+	}
+	
+	@GetMapping("/bbok/update")
+	@ResponseBody
+	public String update() {
+		return "update";
 	}
 
 }
